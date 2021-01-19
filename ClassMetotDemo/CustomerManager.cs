832814f8df30025a999ce02082b1f79f9ca35502
rehawild <76213088+rehawild@users.noreply.github.com>
2021-01-19 22:17:35 +0300

    Add project files.

---
+++ ClassMetotDemo/CustomerManager.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class CustomerManager
+    {
+        public void Add(Customer customer)
+        {
+            Console.WriteLine( customer.Name +" "+ customer.Surname +
+                " isimli müşteri listenize eklendi ");
+        }
+
+        public void Delete(Customer customer)
+        {
+            Console.WriteLine( customer.Name +" "+ customer.Surname +
+                " isimli müşteri listenizden silindi ");
+        }
+
+        public void Make_a_List(Customer[] customers)
+        {
+            //I MADE TWO DIFFERENT LOOPS FOR A BETTER UNDERSTANDING
+            
+            for (int i = 0; i < customers.Length; i++)
+            {
+                Console.WriteLine("\n-----------------------------");
+                Console.WriteLine(i+1 + ".Müşterinin Adı: " + customers[i].Name);
+                Console.WriteLine(i+1 + ".Müşterinin Soyadı: " + customers[i].Surname);
+                Console.WriteLine(i+1 + ".Müşterinin Id'si: " + customers[i].Id);
+                Console.WriteLine(i+1 + ".Müşterinin Hesaptaki Toplam Para Miktarı : " + customers[i].Funds);
+                Console.WriteLine("-----------------------------\n");
+            } 
+
+            /*
+            foreach (Customer custom in customers)
+            {
+                Console.WriteLine("-----------------------------");
+                Console.WriteLine("Müşterinin Adı: " +  custom.Name);
+                Console.WriteLine("Müşterinin Soyadı: " + custom.Surname);
+                Console.WriteLine("Müşterinin Id'si: " + custom.Id);
+                Console.WriteLine("Müşterinin Hesaptaki Toplam Para Miktarı: " + custom.Funds);
+                Console.WriteLine("-----------------------------");
+            }
+            */
+        }
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer customer1 = new Customer();
+            customer1.Name = "Metin Reha";
+            customer1.Surname = "Ermetin";
+            customer1.Id = 3169420;
+            customer1.Funds = 666;
+
+            Customer customer2 = new Customer();
+            customer2.Name = "Buğra Taşkın";
+            customer2.Surname = "Yalçın";
+            customer2.Id = 67567555;
+            customer2.Funds = 313166;
+
+            Customer[] customerarray = new Customer[] { customer1, customer2 };
+            
+
+            //Methods
+            CustomerManager custommanager = new CustomerManager();
+            custommanager.Add(customer1);
+            Console.WriteLine("--------------customer1 add---------------\n");
+
+            custommanager.Delete(customer1);
+            Console.WriteLine("-------------customer1 delete----------------\n");
+
+            custommanager.Add(customer2);
+            Console.WriteLine("---------------customer2 add--------------\n");
+
+            custommanager.Delete(customer2);
+            Console.WriteLine("-------------customer2 delete----------------\n");
+
+            custommanager.Make_a_List(customerarray);
+            Console.WriteLine("------customer1 and customer2 make a list------");
+
+        }
+    }
+}
